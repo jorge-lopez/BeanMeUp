107a44bb796e60f1a8d352d496af75eb02370e42
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-04-27 17:18:08 -0700

    Login y Login Diseño
    Agregue el php para hacer login con credenciales que recibe de la forma,
    falta conectarla con la base de datos, pero en si hace login hardcodeado
    e hice varios cambios en el html index.

---
+++ API's/login.php
@@ -0,0 +1,14 @@
+<?php
+
+$username = $_POST['User'];
+$password = $_POST['Pass'];
+
+if($username == "admin@gmail.com" && $password == "12345"){
+	echo "Succesful Login";
+}
+else{
+	echo "Incorrect Credentials";
+}
+
+
+?>
