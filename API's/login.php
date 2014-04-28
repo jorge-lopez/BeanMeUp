c0f3d943cf4337e0d625834c3f823d5c344576a1
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-04-28 11:35:55 -0700

    Server Connection
    Ya conecte el servidor con el servicio web de login

--- API's/login.php
+++ API's/login.php
@@ -3,6 +3,14 @@
 $username = $_POST['User'];
 $password = $_POST['Pass'];
 
+$connection = mysqli_connect("31.170.166.32","beanmeup");
+if($connection){
+	echo "Connection Succesful\n";
+}
+else{
+	echo "Failed to connect";
+}
+
 if($username == "admin@gmail.com" && $password == "12345"){
 	echo "Succesful Login";
 }
