a705fe1545d16ae746a9e32f07e21d3ec58f3619
Jorge Lopez <jorge.lopez.18820@gmail.com>
2014-05-02 15:04:43 -0700

    Primera Fase Manera de Compra
    tiene algunos bugs y lso valores que se usan son dummys para simular una
    venta pero se prcuro trabajar en el js mas que en el css en esta fase

---
+++ Front-End2/MainPage/js/scripts.js
@@ -0,0 +1,18 @@
+
+var subtotal = 0;
+
+function AgregarProducto(padre) {
+
+
+	var producto = $(padre).children("label").text();
+	var precio = parseInt($(padre).children("input").val());
+
+	$("#venta").append( producto + "  $" + precio + "<br>" );	
+
+	subtotal = subtotal + precio;
+	var iva = (subtotal * .16).toFixed(2);
+	var total = (subtotal * 1.16).toFixed(2);
+	document.getElementById("total").innerHTML = "SubTotal: " + subtotal + " <br>" + 
+							"Iva: " + iva + " <br>" + 
+							"Total: " + total;
+}
