897be00e58f819baec37617a3ecc49b6eef89912
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-05-02 10:51:28 -0700

    Sessions
    Agregue Sessions al hacer login y se arreglo bug que te dejaba entrar si
    no ponias ninguna credencial

--- API's/login.php
+++ API's/login.php
@@ -5,15 +5,21 @@ $connection = mysql_connect("mysql.fhero.net", "u304295155_hdcde", "hardcode123"
 mysql_select_db("u304295155_bnmup") or die("cannot connect to the database" . mysql_error());
 $SelectEmailQuery = "SELECT * FROM tblemployee WHERE Email='$username' AND Password='$password'";
 $Result = mysql_query($SelectEmailQuery);
-
+// $Result ="call sp_login('".$username."','".$password."');";
+// $query = mysql_query($Result);
+//$Result = mysql_query("CALL sp_login('admin@gmail.com','12345')");
 while ($row = mysql_fetch_assoc($Result)) {
 	$dbEmail = $row['Email'];
 	$dbPassword = $row['Password'];
+	$dbFirstName = $row['FirstName'];
 }
-
-if ($username == $dbEmail && $password == $dbPassword) {
-	$encryptedPassword = md5($dbPassword);
-	echo "Succesful Login\n";
+if( empty($_POST['User']) && empty($_POST['Pass']) ) {
+	header('Location: index.html' );
+}
+else if ($username == $dbEmail && $password == $dbPassword) {
+	//$encryptedPassword = md5($dbPassword);
+	session_start();
+	$_SESSION["Name"] = $dbFirstName;
 	header('Location: MainPage/main.html' );
 }
 else {
