0d467226af08db49b564d2ad2d5d60ed3c466e9b
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-05-06 09:26:25 -0700

    Login Stored Procedure
    Se agrego el store procedure para el login.php

--- API's/login.php
+++ API's/login.php
@@ -4,12 +4,9 @@ $username = $_POST['User'];
 $password = $_POST['Pass'];
 $connection = mysql_connect("mysql.fhero.net", "u304295155_hdcde", "hardcode123", "u304295155_bnmup") or die('Could not connect to mysql server.');
 mysql_select_db("u304295155_bnmup") or die("cannot connect to the database" . mysql_error());
-$SelectEmailQuery = "SELECT * FROM tblemployee WHERE Email='$username' AND Password='$password'";
-$Result = mysql_query($SelectEmailQuery);
-// $Result ="call sp_login('".$username."','".$password."');";
-// $query = mysql_query($Result);
-//$Result = mysql_query("CALL sp_login('admin@gmail.com','12345')");
-while ($row = mysql_fetch_assoc($Result)) {
+$LoginCredentials ="call sp_Login('$username','$password');";
+$LoginQuery = mysql_query($LoginCredentials);
+while ($row = mysql_fetch_assoc($LoginQuery)) {
 	$dbEmail = $row['Email'];
 	$dbPassword = $row['Password'];
 	$dbFirstName = $row['FirstName'];
