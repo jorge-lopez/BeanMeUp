802632135e40a5a80fd2ce2a79f9eedcca177b60
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-05-24 20:01:28 -0700

    Array de Generar ticket

--- Front-End2/site/Cajero/php/GenerarTicket.php
+++ Front-End2/site/Cajero/php/GenerarTicket.php
@@ -1,6 +1,5 @@
 <?php
-$connection = mysql_connect("mysql.fhero.net", "u304295155_hdcde", "hardcode123", "u304295155_bnmup") or die('Could not connect to mysql server.');
-mysql_select_db("u304295155_bnmup") or die("cannot connect to the database" . mysql_error());
-
-
+include '../../db_connect.php';
+$Ticket = $_POST['ticket'];
+$TicketArray = json_decode($Ticket,true);
 ?>
