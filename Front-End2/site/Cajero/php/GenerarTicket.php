93d96edf7e1e054ae661037662616c39221a2ec2
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-05-25 22:47:12 -0700

    Generar Ticket V2
    -Se Inserta a tabla Ticket
    -Se Inserta a table detallesTicket
    -Ya existe relacion entre estas dos

--- Front-End2/site/Cajero/php/GenerarTicket.php
+++ Front-End2/site/Cajero/php/GenerarTicket.php
@@ -5,10 +5,28 @@ $Ticket = $_POST['ticket'];
 $TicketArray = json_decode($Ticket,true);
 $Precio = $_POST['detallesTotal'];
 $dbEmployeeID = $_SESSION["EmployeeID"];
+$InsertTicketSP ="call sp_Ticket('0','$dbEmployeeID','100.00');";
+$InsertTicketQuery = mysql_query($InsertTicketSP) or die(mysql_error());;
+mysql_close();
+
+include '../../db_connect.php';
+$SelectTicketID = mysql_query('SELECT TicketID FROM tblTicket ORDER BY TicketID DESC LIMIT 1') or die (mysql_error());
+$row = mysql_fetch_assoc($SelectTicketID);
+$dbTicketID = $row['TicketID'];
+mysql_close();
 
 foreach($TicketArray as $item) { //foreach element in $arr
+include '../../db_connect.php';
+$ProductId = $item['ProductId'];
+$SizeId = $item['SizeId'];
+$Cantidad = $item['Cantidad'];
+$InsertTicketDetailSP ="call sp_TicketDetail('0','$dbTicketID','$ProductId','$SizeId','$Cantidad');";
+$InsertTicketDetailQuery = mysql_query($InsertTicketDetailSP) or die(mysql_error());;
+
     echo "ProductId : " . $item['ProductId'] 
     	. ", SizeId : " . $item['SizeId'] 
 		. ", Cantidad : " . $item['Cantidad'] . "\n"; //etc
 }
+mysql_close();
+
 ?>
