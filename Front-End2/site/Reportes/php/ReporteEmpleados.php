c7697e06e9ccbc98c5ab1b9546c104de03b2a804
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-05-27 23:52:51 -0700

    Reporte Empleados PHP
    -Ya regresa toda la informacion del SP de los empleados y lo guarda en
    JSON

--- Front-End2/site/Reportes/php/ReporteEmpleados.php
+++ Front-End2/site/Reportes/php/ReporteEmpleados.php
@@ -1,11 +1,26 @@
 <?php
 include '../../db_connect.php';
-$SelectEmployeeSP = "call sp_Employee_List();";
-$SelectEmployeeQuery = mysql_query($SelectEmployeeSP);
-while ($FieldNames = mysql_fetch_field($SelectEmployeeQuery)){
+
+$SelectEmployeeFieldSP = "call sp_Employee_List();";
+$SelectEmployeeFieldQuery = mysql_query($SelectEmployeeFieldSP);
+
+while ($FieldNames = mysql_fetch_field($SelectEmployeeFieldQuery)) {
 	$FieldNamesArray[] = $FieldNames->name;
 }
+
 $FieldNamesJson = json_encode($FieldNamesArray);
-	echo $FieldNamesJson;
+echo $FieldNamesJson;
+mysql_close();
+include '../../db_connect.php';
+
+$EmployeeSP = "call sp_Employee_List();";
+$EmployeeQuery = mysql_query($EmployeeSP);
+
+while ($row = mysql_fetch_assoc($EmployeeQuery)) {
+	$Employees[] = $row;
+}
+
+$EmployeesData = json_encode($Employees);
+echo $EmployeesData;
 mysql_close();
 ?>
