403bc9e4602ba7cd4c2fd89b996c4c1767b2178b
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-05-29 16:25:53 -0700

    Registro Proveedores Update

--- Front-End2/site/Registro/Registrys/registerProviders.php
+++ Front-End2/site/Registro/Registrys/registerProviders.php
@@ -4,26 +4,21 @@ $NombreProveedor = $_POST['NombreProveedor'];
 $NombreCompania = $_POST['NombreCompania'];
 $Email = $_POST['Email'];
 $Telefono = $_POST['Telefono'];
-$Celular = $_POST['Ceular'];
+$Celular = $_POST['Celular'];
 $Direccion = $_POST['Direccion'];
 
-if( empty($_POST['FName']) && empty($_POST['LName']) && empty($_POST['Telephone']) && empty($_POST['Email']) && empty($_POST['Address']) && empty($_POST['Password']) && empty($_POST['VPassword']) && empty($_POST['Gender']) && empty($_POST['Salary']) && empty($_POST['Registro'])) {
-	header('Location: RegistryEmployees.html' );
+if( empty($_POST['NombreProveedor']) && empty($_POST['NombreCompania']) && empty($_POST['Email']) && empty($_POST['Telefono']) && empty($_POST['Celular']) && empty($_POST['Direccion'])) {
+	header('Location: RegistryProviders.html' );
 }
 else{
 if(!filter_var($Email, FILTER_VALIDATE_EMAIL)){
 	$message = "Invalid email address please type a valid email.";
 }
 else{
-	if($Password == $VPassword){
 		include '../../db_connect.php';
-		$sql = mysql_query("CALL sp_Employee('0','$Registrar','$FirstName','$LastName','$Selected_radio','$Phone','$Address','$Email','$Password','$Salary');")or die(mysql_error());
+		$sql = mysql_query("CALL sp_Provider('0','$NombreProveedor','$Direccion','$Telefono','$Celular','$NombreCompania','$Email');")or die(mysql_error());
 		echo "Se agrego";
 		mysql_close($connection);
-	}
-	else{
-		header('Location: RegistryEmployees.html' );
-	}
 }
 }
 ?>
