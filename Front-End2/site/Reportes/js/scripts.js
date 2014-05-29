e3e5a7b91f74cdd038c99ee23063680da4ffbd6d
Jorge Lopez <jorge.lopez.18820@gmail.com>
2014-05-29 02:13:28 -0700

    First Report
    -Created 2 phps (One for columns and another for tableData)
    -Modified js to fill tables
    -Know Bugs
    *Can't width for select columns

--- Front-End2/site/Reportes/js/scripts.js
+++ Front-End2/site/Reportes/js/scripts.js
@@ -1,21 +1,17 @@
 
-function ObtenerColumnas(){
- 	
- 	var columnas;
-	var jqXHR = $.ajax({
-     	url: "php/ReporteEmpleados.php",
-		success: function(data) {                
-            columnas = data;
-        }
-      });
-   jqXHR.done(function () {
-   		AgregarColumnasHTML(columnas);
-   });
+function ObtenerDatos(){
 
+	$.get("php/ReporteEmpleados.php", function(data) {
+	         AgregarColumnasHTML(data);      
+	});
+	$.get("php/InformacionEmpleados.php", function(data) {
+	         LlenarTabla(data);      
+	});
 }
 function AgregarColumnasHTML(ColumasJSON){
 	
 	var columnas = $.parseJSON(ColumasJSON);
+	
 	var width = 100/columnas.length;
 	var html = "<thead><tr>";
 
@@ -25,16 +21,19 @@ function AgregarColumnasHTML(ColumasJSON){
 
 	html += "</tr></thead>";
 	$("table#reporte").append(html);
-	LlenarTabla(columnas.length);
 }
-function LlenarTabla(NumeroColumnas){
 
+function LlenarTabla(TuplasJSON){
+
+	var tuplas = $.parseJSON(TuplasJSON);
+	var NumeroFilas = tuplas.length;
 	var html = "<tbody>";    
     
     for (var i = 0; i < 3; i++) {
     	html += "<tr>";	
-		for (var j = 0; j < NumeroColumnas; j++) {
-			html += '<td>Content Goes Here</td>';    
+    	var temp = tuplas[i];
+		for (var key in temp) {
+			html += '<td>' + temp[key] +'</td>';    
 		}
 		html += "</tr>";
 	}
--- Front-End2/site/Reportes/php/ReporteEmpleados.php
+++ Front-End2/site/Reportes/php/ReporteEmpleados.php
@@ -1,4 +1,5 @@
 <?php
+
 include '../../db_connect.php';
 
 $SelectEmployeeFieldSP = "call sp_Employee_List();";
@@ -7,20 +8,8 @@ $SelectEmployeeFieldQuery = mysql_query($SelectEmployeeFieldSP);
 while ($FieldNames = mysql_fetch_field($SelectEmployeeFieldQuery)) {
 	$FieldNamesArray[] = $FieldNames->name;
 }
-
-$FieldNamesJson = json_encode($FieldNamesArray);
-echo $FieldNamesJson;
+$FielNamesJSON = json_encode ($FieldNamesArray);
+echo $FielNamesJSON;
 mysql_close();
-include '../../db_connect.php';
 
-$EmployeeSP = "call sp_Employee_List();";
-$EmployeeQuery = mysql_query($EmployeeSP);
-
-while ($row = mysql_fetch_assoc($EmployeeQuery)) {
-	$Employees[] = $row;
-}
-
-$EmployeesData = json_encode($Employees);
-echo $EmployeesData;
-mysql_close();
 ?>
