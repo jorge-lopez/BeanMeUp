2e3ddb169e1ec595e0d9d2a92f87b58ebb3fd9d8
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-05-30 16:17:17 -0700

    Cambio nombres
    se cambio nombres al php de registros

--- Front-End2/site/Actualizar/php/InsertPromociones.php
+++ Front-End2/site/Actualizar/php/InsertPromociones.php
@@ -1,8 +1,9 @@
 <?php
 include '../../db_connect.php';
-$StockName = $_POST['StockName'];
-$Quantity = $_POST['Quantity'];
-$sql = mysql_query("CALL sp_Stock('0','$StockName','$Quantity')");
+$ExpirationDate = $_POST['ExpirationDate'];
+$Discount = $_POST['Discount'];
+$Description = $_POST['Description'];
+$sql = mysql_query("CALL sp_Coupon('0','$ExpirationDate','$Discount','$Description')");
 mysql_close();
 
 ?>
--- Front-End2/site/Registro/Registrys/php/InsertStock.php
+++ Front-End2/site/Registro/Registrys/php/InsertStock.php
@@ -2,12 +2,7 @@
 include '../../db_connect.php';
 $StockName = $_POST['StockName'];
 $Quantity = $_POST['Quantity'];
+$sql = mysql_query("CALL sp_Stock('0','$StockName','$Quantity')");
+mysql_close();
 
-if( empty($_POST['StockName']) && empty($_POST['Quantity'])) {
-	header('Location: RegistryProviders.html' );
-}
-else{
-	$sql = mysql_query("CALL sp_Stock('0','$StockName','$Quantity')");
-	mysql_close();
-}
 ?>
