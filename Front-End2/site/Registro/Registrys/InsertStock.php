b52afcee08792dceb98f54e365a6e21d8814aee5
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-05-30 01:37:42 -0700

    Se movio a Registros

---
+++ Front-End2/site/Registro/Registrys/InsertStock.php
@@ -0,0 +1,13 @@
+<?php
+include '../../db_connect.php';
+$StockName = $_POST['StockName'];
+$Quantity = $_POST['Quantity'];
+
+if( empty($_POST['StockName']) && empty($_POST['Quantity'])) {
+	header('Location: RegistryProviders.html' );
+}
+else{
+	$sql = mysql_query("CALL sp_Stock('0','$StockName','$Quantity')");
+	mysql_close();
+}
+?>
