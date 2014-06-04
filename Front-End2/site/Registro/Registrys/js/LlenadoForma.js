dd2346ec803760de5890c9dbc9fae95a0df1c032
Jorge Lopez <jorge.lopez.18820@gmail.com>
2014-06-04 03:09:32 -0700

    Promotions Registry Online
    -DatePicker.js added
    -promotions view ready for .php's

--- Front-End2/site/Registro/Registrys/js/LlenadoForma.js
+++ Front-End2/site/Registro/Registrys/js/LlenadoForma.js
@@ -2,50 +2,66 @@ function LlenarCampos(Funcion, Id, Actor){
 	
 	if(Funcion != "Insert"){    		
 		$.post("php/" + Actor + "Get.php", {ID : Id}, function(data) {
-		         LlenarForma(data);      
+		         LlenarForma(data, Actor);      
 		});
 	}
 }
-function LlenarForma(InfoJSON){
+function LlenarForma(InfoJSON, Actor){
 	var Info = $.parseJSON(InfoJSON);
 	var Objeto = Info[0];
 
-	//Empleado y Campos Comunes
-	$("#Password").prop("type", "text");
-	$("#VPassword").prop("type", "text");
+	switch(Actor) {
+    case "Empleado":
+    	//Empleado y Campos Comunes
+		$("#Password").prop("type", "text");
+		$("#VPassword").prop("type", "text");
+			
+		$("#FirstName").val(Objeto["FirstName"]);
+		$("#LastName").val(Objeto["LastName"]);
 		
-	$("#FirstName").val(Objeto["FirstName"]);
-	$("#LastName").val(Objeto["LastName"]);
-	
-	$("#Email").val(Objeto["Email"]);
-	$("#phone").val(Objeto["Phone"]);
-	$("#salary").val(Objeto["Salary"]);
-	$("#Address").val(Objeto["Address"]);
+		$("#Email").val(Objeto["Email"]);
+		$("#phone").val(Objeto["Phone"]);
+		$("#salary").val(Objeto["Salary"]);
+		$("#Address").val(Objeto["Address"]);
 
-	if(Objeto["Gender"] == "Male")
-		$("#Male").prop("checked", true);
-	else
-		$("#Female").prop("checked", true);
+		if(Objeto["Gender"] == "Male")
+			$("#Male").prop("checked", true);
+		else
+			$("#Female").prop("checked", true);
 
-	if(Objeto["PositionID"] == "1")
-		$("#Cajero").prop("checked", true);
-	else if(Objeto["PositionID"] == "2")
-		$("#Supervisor").prop("checked", true);
-	else if(Objeto["PositionID"] == "3")
-		$("#Gerente").prop("checked", true);
+		if(Objeto["PositionID"] == "1")
+			$("#Cajero").prop("checked", true);
+		else if(Objeto["PositionID"] == "2")
+			$("#Supervisor").prop("checked", true);
+		else if(Objeto["PositionID"] == "3")
+			$("#Gerente").prop("checked", true);
 
 
-	$("#Password").val(Objeto["Password"]);	
-	$("#VPassword").val(Objeto["Password"]);
+		$("#Password").val(Objeto["Password"]);	
+		$("#VPassword").val(Objeto["Password"]);
 
-	//Proveedor
-	$("#FirstName").val(Objeto["ProviderName"]);
-	$('input[name=NombreCompania]').val(Objeto["ProviderCompany"]);
-	$("#Email").val(Objeto["ProviderEmail"]);
-	$("#phone").val(Objeto["ProviderPhone"]);
-	$("#phone2").val(Objeto["ProviderCellphone"]);
-	$("#Address").val(Objeto["ProviderAddress"]);
+        break;
+    case "Proveedor":
+        //Proveedor
+		$("#FirstName").val(Objeto["ProviderName"]);
+		$('input[name=NombreCompania]').val(Objeto["ProviderCompany"]);
+		$("#Email").val(Objeto["ProviderEmail"]);
+		$("#phone").val(Objeto["ProviderPhone"]);
+		$("#phone2").val(Objeto["ProviderCellphone"]);
+		$("#Address").val(Objeto["ProviderAddress"]);
 
-	///PROCEDURE u304295155_bnmup.sp_Coupon_ByID does not exist
-	
+        break;
+
+	case "Promociones":
+		//Promociones
+		$("#descuento").val(Objeto["Discount"]);
+		$('input[name=Description]').val(Objeto["Description"]);
+		
+		var fechaRAW = Objeto["ExpirationDate"].split("-");
+		var fecha = fechaRAW[2] + "/" + fechaRAW[1] + "/" + fechaRAW[0];
+		$('input[name=ExpirationDate]').val(Objeto["ExpirationDate"]);
+
+    default:
+        break;
+    }
 }
--- Front-End2/site/Registro/Registrys/js/test.js
+++ Front-End2/site/Registro/Registrys/js/test.js
@@ -1,5 +1,8 @@
 jQuery(function($){
-   $(".phone").mask("(999) 999-9999");
-    $("#salary").mask("9999999",{placeholder:""});
-   $("#ssn").mask("999-99-9999");
+	$(".phone").mask("(999) 999-9999");
+	$("#salary").mask("9999999",{placeholder:""});
+	$("#ssn").mask("999-99-9999");
+	$('#descuento').mask("99");
+	$("#dpd1").mask("9999-99-99");
+	$("#dpd2").mask("9999-99-99");
 });;
