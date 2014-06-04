012abdf16e5da0ac3ce8a40faadf186e072dba5f
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-06-04 09:42:38 -0700

    Se agrego un campo extra al php

--- Front-End2/site/Registro/Registrys/php/PromocionesInsert.php
+++ Front-End2/site/Registro/Registrys/php/PromocionesInsert.php
@@ -1,13 +1,14 @@
 <?php
 include '../../../db_connect.php';
 $ExpirationDate = $_POST['ExpirationDate'];
+$IssueDate = $_POST['IssueDate'];
 $Discount = $_POST['Discount'];
 $Description = $_POST['Description'];
 if( empty($_POST['ExpirationDate']) && empty($_POST['Discount']) && empty($_POST['Description'])) {
 	header('Location: RegistryProviders.html' );
 }
 else{
-	$sql = mysql_query("CALL sp_Coupon('0','$ExpirationDate','$Discount','$Description')");
+	$sql = mysql_query("CALL sp_Coupon('0','$IssueDate,'$ExpirationDate','$Discount','$Description')");
 	mysql_close();
 }
 ?>
--- Front-End2/site/Registro/Registrys/php/PromocionesUpdate.php
+++ Front-End2/site/Registro/Registrys/php/PromocionesUpdate.php
@@ -2,13 +2,14 @@
 include '../../../db_connect.php';
 $ID = $_POST['ID'];
 $ExpirationDate = $_POST['ExpirationDate'];
+$IssueDate = $_POST['IssueDate'];
 $Discount = $_POST['Discount'];
 $Description = $_POST['Description'];
 if( empty($_POST['ExpirationDate']) && empty($_POST['Discount']) && empty($_POST['Description'])) {
 	header('Location: RegistryProviders.html' );
 }
 else{
-	$sql = mysql_query("CALL sp_Coupon('$ID','$ExpirationDate','$Discount','$Description')");
+	$sql = mysql_query("CALL sp_Coupon('$ID','$IssueDate','$ExpirationDate','$Discount','$Description')");
 	mysql_close();
 }
 ?>
