24cd6334508b34a794056dc44087aaec1966a0cd
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-06-10 15:18:52 -0700

    fix SP

--- Front-End2/site/Registro/Registrys/php/InventarioUpdate.php
+++ Front-End2/site/Registro/Registrys/php/InventarioUpdate.php
@@ -2,8 +2,8 @@
 include '../../../db_connect.php';
 session_start();
 $ID = $_POST['ID'];
-$ProviderID = $_POST['ProviderID'];
-// $StockName = $_POST['StockName'];
+//$ProviderID = $_POST['ProviderID'];
+$StockName = $_POST['StockName'];
 // $Quantity = $_POST['Quantity'];
 $UnitID = $_POST['UnitID'];
 
