a634c5c23d668cceb71c2c50d79ba930bb2bc5ec
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-06-10 15:18:18 -0700

    Se modifico el insert y update de inventario

--- Front-End2/site/Registro/Registrys/php/InventarioInsert.php
+++ Front-End2/site/Registro/Registrys/php/InventarioInsert.php
@@ -1,11 +1,11 @@
 <?php
 include '../../../db_connect.php';
 session_start();
-$ProviderID = $_POST['ProviderID'];
+//$ProviderID = $_POST['ProviderID'];
 $StockName = $_POST['StockName'];
-$Quantity = $_POST['Quantity'];
+//$Quantity = $_POST['Quantity'];
 $UnitID = $_POST['UnitID'];
 
-$InsertStockQuery = mysql_query("CALL sp_Stock('0','$ProviderID','$StockName','$Quantity','$UnitID');");
+$InsertStockQuery = mysql_query("CALL sp_Stock('0','$StockName','$UnitID');");
 mysql_close();
 ?>
--- Front-End2/site/Registro/Registrys/php/InventarioUpdate.php
+++ Front-End2/site/Registro/Registrys/php/InventarioUpdate.php
@@ -3,10 +3,10 @@ include '../../../db_connect.php';
 session_start();
 $ID = $_POST['ID'];
 $ProviderID = $_POST['ProviderID'];
-$StockName = $_POST['StockName'];
-$Quantity = $_POST['Quantity'];
+// $StockName = $_POST['StockName'];
+// $Quantity = $_POST['Quantity'];
 $UnitID = $_POST['UnitID'];
 
-$InsertStockQuery = mysql_query("CALL sp_Stock('$ID','$ProviderID','$StockName','$Quantity','$UnitID');");
+$InsertStockQuery = mysql_query("CALL sp_Stock('$ID','$StockName','$UnitID');");
 mysql_close();
 ?>
