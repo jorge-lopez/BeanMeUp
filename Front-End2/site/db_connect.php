f37de40b03d7e84469f34a6d8a3b447a280060ff
Jorge Lopez <jorge.lopez.18820@gmail.com>
2015-08-10 17:23:45 -0700

    Store and Detail views
    Store and detail view are finished, using dumm repos and the chrome
    browser

--- Front-End2/site/db_connect.php
+++ Front-End2/site/db_connect.php
@@ -1,4 +1,9 @@
+
 <?php
-$connection = mysql_connect("mysql.fhero.net", "u304295155_hdcde", "hardcode123", "u304295155_bnmup") or die('Could not connect to mysql server.');
+$connection = mysql_connect("localhost", "root", "", "u304295155_bnmup") or die('Could nsssot connect to mysql server.');
 mysql_select_db("u304295155_bnmup") or die("cannot connect to the database" . mysql_error());
 ?>
+<!-- // <?php
+// $connection = mysql_connect("mysql.fhero.net", "u304295155_hdcde", "hardcode123", "u304295155_bnmup") or die('Could not connect to mysql server.');
+// mysql_select_db("u304295155_bnmup") or die("cannot connect to the database" . mysql_error());
+// ?> -->
